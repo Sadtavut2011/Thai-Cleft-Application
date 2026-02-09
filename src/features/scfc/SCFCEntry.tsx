3f6c03da39146c6b6d639ffe6bda986bf5957248
Sadtavut2011 <Sadtavut2011@gmail.com>
2026-02-09 11:23:55 +0700

    sync: Update SCFCEntry.tsx from local

--- src/features/scfc/SCFCEntry.tsx
+++ src/features/scfc/SCFCEntry.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useMediaQuery } from '../../hooks/useMediaQuery';
 import WebDashboard from './scfc-web/dashboard/WebDashboard';
-import MobileLayout from './mobile/layout/MobileLayout';
+import MobileLayout from './scfc-mobile/layout/MobileLayout';
 
 export default function SCFCEntry() {
   const isDesktop = useMediaQuery('(min-width: 1024px)');
